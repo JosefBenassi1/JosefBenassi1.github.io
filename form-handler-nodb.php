6c74e00183c7c2875d4f4ce205cea00a001f4bc6
JosefBenassi1 <benassisolutions@gmail.com>
2017-06-15 02:12:42 +0100

    heloo

--- form-handler-nodb.php
+++ form-handler-nodb.php
@@ -1,20 +1,21 @@
 <?php
-// Emails form data to you and the person submitting the form
-// This version requires no database.
-// Set your email below
-$myemail = "bennyjoe100@hotmail.com"; // Replace with your email, please
+        if(isset($_POST['name']) && !empty($_POST['name'])){
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $phonenum = $_POST['phonenum'];
+                $message = $_POST['message'];
+                $finalMessage = "Name: ".$name.
+                " || Email: ".$email.
+                " || Phone Number: ".$phonenum.
+                " || Message: ".$message;
+//Insert your email here
+                mail("bennyjoe100@hotmail.com", 'Message received from website contact form.', $finalMessage);
+                header("Location: index.html");
+        }
+?>
 
-// Receive and sanitize input
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-
-// set up email
-$msg = "New contact form submission!\nName: " . $name . "\nEmail: " . $email . "\nPhone: " . $phone . "\nEmail: " . $email;
+<!--$msg = "New contact form submission!\nName: " . $name . "\nEmail: " . $email . "\nPhone: " . $phone . "\nEmail: " . $email;
 $msg = wordwrap($msg,70);
 mail($myemail,"New Form Submission",$msg);
 mail($email,"Thank you for your form submission",$msg);
-echo 'Thank you for your submission.  Please <a href="index.html">Click here to return to our homepage.';
-
-?>
+echo 'Thank you for your submission.  Please <a href="index.html">Click here to return to our homepage.'; //-->
